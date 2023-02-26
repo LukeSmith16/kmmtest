343c62a1545b491505a460cdafd290a69d66799a
Luke Smith <lukeedwardsmith97@gmail.com>
2023-02-26 12:22:50 +0700

    wd

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "shared",
-            path: "./shared.xcframework"
+            path: "./shared/build/XCFrameworks/debug/shared.xcframework"
         ),
     ]
 )
