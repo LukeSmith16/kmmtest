e3df1a39b23d9c3a645a82901ca7bb4d62867ed4
Luke Smith <lukeedwardsmith97@gmail.com>
2023-02-26 18:40:38 +0700

    try again

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,60 +5,44 @@ plugins {
 }
 
 kotlin {
-    android {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
-        }
-    }
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "shared"
-            isStatic = true
-        }
-    }
-
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-        val androidMain by getting
-        val androidUnitTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
-    }
-
     multiplatformSwiftPackage {
         swiftToolsVersion("5.3")
         targetPlatforms {
             iOS { v("13") }
         }
+        packageName("shared")
         outputDirectory(File(rootDir, "/"))
+    }
+
+    android()
+
+    ios {
+        binaries {
+            framework {
+                baseName = "shared"
+                isStatic = false
+            }
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries {
+            framework {
+                baseName = "shared"
+                isStatic = false
+            }
+        }
+    }
+
+    sourceSets {
+        val iosMain by getting
+        val iosTest by getting
+        val iosSimulatorArm64Main by getting {
+            dependsOn(iosMain)
+        }
+        val iosSimulatorArm64Test by getting {
+            dependsOn(iosTest)
+        }
     }
 }
 
