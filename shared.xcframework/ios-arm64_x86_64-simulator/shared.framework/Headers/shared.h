f0f7edf36cd4ddb93da828e4fb7e571f7099e925
Luke Smith <lukeedwardsmith97@gmail.com>
2023-02-26 22:20:20 +0700

    new method

--- shared.xcframework/ios-arm64_x86_64-simulator/shared.framework/Headers/shared.h
+++ shared.xcframework/ios-arm64_x86_64-simulator/shared.framework/Headers/shared.h
@@ -147,6 +147,7 @@ __attribute__((swift_name("Greeting")))
 @interface SharedGreeting : SharedBase
 - (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
 + (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (int32_t)doSomethingCool __attribute__((swift_name("doSomethingCool()")));
 - (NSString *)greet __attribute__((swift_name("greet()")));
 @end
 
