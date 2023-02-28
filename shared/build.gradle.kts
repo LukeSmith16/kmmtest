1c5575732d018ceb58046fe4fd128904828a6677
Luke Smith <lukeedwardsmith97@gmail.com>
2023-02-28 16:08:48 +0700

    3ef

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -16,17 +16,13 @@ kotlin {
         }
     }
 
-    iosSimulatorArm64()
-    iosArm64()
+        iosSimulatorArm64()
+        iosArm64()
 
     sourceSets {
         val iosMain by getting
-        val iosTest by getting
         val iosSimulatorArm64Main by getting {
             dependsOn(iosMain)
-        }
-        val iosSimulatorArm64Test by getting {
-            dependsOn(iosTest)
         }
     }
 }
