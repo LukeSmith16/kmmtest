2c4d766bcaa93a96faa100b644abb38bd976f013
Luke Smith <lukeedwardsmith97@gmail.com>
2023-03-06 18:39:09 +0700

    something

--- Package.swift
+++ Package.swift
@@ -1,21 +1,26 @@
-// swift-tools-version:5.7.0
 import PackageDescription
 
+let remoteKotlinUrl = "https://maven.pkg.github.com/buxapp/shared-stocks-mobile/Stocks/shared-kmmbridge/0.1.5/shared-kmmbridge-0.1.5.zip"
+let remoteKotlinChecksum = "7787bf5a7ed0cf318dd115fcc9d87d8764c1f416cb4d10377b9aa166abe676b8"
+let packageName = "shared"
+
 let package = Package(
-    name: "shared",
+    name: packageName,
     platforms: [
-        .iOS(.v15)
+        .iOS(.v13)
     ],
     products: [
         .library(
-            name: "shared",
-            targets: ["shared"]
+            name: packageName,
+            targets: [packageName]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "shared",
-            path: "./shared.xcframework"
-        ),
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
     ]
 )
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,56 +1,66 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
-    id("io.github.luca992.multiplatform-swiftpackage") version "2.1.1"
+    id("co.touchlab.faktory.kmmbridge") version "0.3.5"
+    `maven-publish`
 }
 
 kotlin {
     android()
-    ios {
-        binaries {
-            framework {
-                baseName = "shared"
-                isStatic = true
-            }
-        }
-    }
 
-    iosSimulatorArm64 {
-        binaries {
-            framework {
-                baseName = "shared"
-                isStatic = true
-            }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
         }
     }
 
     sourceSets {
-        val iosMain by getting
-        val iosTest by getting
-        val iosSimulatorArm64Main by getting {
-            dependsOn(iosMain)
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
         }
-        val iosSimulatorArm64Test by getting {
-            dependsOn(iosTest)
+        val androidMain by getting
+        val androidUnitTest by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
-}
-
-multiplatformSwiftPackage {
-    swiftToolsVersion("5.7.0")
-    targetPlatforms {
-        iOS { v("15") }
-    }
-    packageName("shared")
-    outputDirectory(File(rootDir, "/"))
-    distributionMode { local() }
 }
 
 android {
     namespace = "com.example.myapplication"
     compileSdk = 33
     defaultConfig {
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 33
     }
+}
+
+addGithubPackagesRepository()
+kmmbridge {
+    mavenPublishArtifacts()
+    githubReleaseVersions()
+    versionPrefix.set("0.1")
+    spm()
 }
